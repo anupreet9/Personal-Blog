cb34f202a98d594638c89dbf355fcbd12256871e
anupreet9 <anupreet.aashy@gmail.com>
2022-06-11 14:58:30 +0530

    Update common.js

--- client/src/services/common.js
+++ client/src/services/common.js
@@ -3,7 +3,7 @@ import utf8 from "utf8";
 
 const info = {
   token: encode(utf8.encode(encode(`Bearer ${process.env.REACT_APP_API_KEY}`))),
-  proxy: "https://anupreet9.github.io/Blog/client/build/index.html#/"
+  proxy: "https://curlyhairedescapade.herokuapp.com"
 }
 
 export default info;
